98a3a05593bfdd61efaf1df7c0e3fd8f61f89dd7
Kauê Soares <kaue222120@gmail.com>
2025-05-01 16:10:06 -0300

    starting loop

--- project/src/App.java
+++ project/src/App.java
@@ -1,26 +1,21 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Path path = Path.of("input.txt");
 
-        Queue<Process> processes = new LinkedList<>(
-                Files.lines(path)
-                        .map(line -> {
-                            String[] split = line.split(";");
-                            return new Process(
-                                    split[0],
-                                    Integer.parseInt(split[1]),
-                                    Integer.parseInt(split[2]),
-                                    Integer.parseInt(split[3]));
-                        })
-                        .sorted((a, b) -> a.time() - b.time())
-                        .toList());
+        Queue<Process> processes = parseFileContentIntoProcesses(path);
+
+        System.out.println("Processes (file content):");
+        processes.forEach(System.out::println);
 
         Map<Integer, Queue<Process>> queuesByPriority = processes.stream()
                 .map(Process::priority)
@@ -29,6 +24,34 @@ public class App {
                         priority -> priority,
                         priority -> new LinkedList<>()));
 
-        // while (!processes.isEmpty() && queuesByPriority.keySet().stream().anyMatch((p, queue) -> )))
+        int currentTime = 0;
+
+        while (true) {
+            boolean missingProcesses = !processes.isEmpty();
+            boolean allQueuesAreEmpty = queuesByPriority.entrySet().stream().anyMatch(entry -> !entry.getValue().isEmpty());
+
+            // TODO: validate if all bursts have been spent
+
+            if (!missingProcesses && !allQueuesAreEmpty) break;
+
+        }
+    }
+
+    private static Queue<Process> parseFileContentIntoProcesses(Path path) throws IOException {
+        try (Stream<String> lines = Files.lines(path)) {
+            return new LinkedList<>(
+                    lines
+                            .map(line -> {
+                                String[] split = line.split(";");
+                                return new Process(
+                                        split[0],
+                                        Integer.parseInt(split[1]),
+                                        Integer.parseInt(split[2]),
+                                        Integer.parseInt(split[3]));
+                            })
+                            .sorted(Comparator.comparingInt(Process::time))
+                            .toList()
+            );
+        }
     }
 }
