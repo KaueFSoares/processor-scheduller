914afe60b1cce7e74dbf20d84b335f62b10f876e
Kauê Soares <kaue222120@gmail.com>
2025-05-11 19:08:35 -0300

    a

--- project/src/App.java
+++ project/src/App.java
@@ -21,6 +21,66 @@ public class App {
                 .distinct()
                 .collect(Collectors.toMap(priority -> priority, priority -> new LinkedList<>()));
 
+        int currentTime = -1;
+        Process last = null;
+        int runningFor = 0;
+
+        while (!shouldStop(processes, queuesByPriority)) {
+            currentTime++;
+
+            putProcessesToPriorityQueue(processes, currentTime, queuesByPriority);
+
+            Queue<Process> highestPriorityNonEmptyQueue = queuesByPriority.entrySet().stream()
+                    // removes empty queues
+                    .filter(entry -> !entry.getValue().isEmpty())
+                    // sorts by priority
+                    .sorted(Comparator.comparingInt(Map.Entry::getKey))
+                    .map(Map.Entry::getValue)
+                    .findFirst()
+                    .orElse(null);
+
+            if (highestPriorityNonEmptyQueue == null) continue;
+
+            Process finalLast = last;
+            Process runningProcess = highestPriorityNonEmptyQueue.stream().filter(p -> p.equals(finalLast)).findFirst().orElse(null);
+
+            if (runningProcess != null) {
+                if (runningProcess.isFinished()) {
+                    // switch to another one if there is another from same queue or else got to another queue
+                }
+
+                if (runningFor == QUANTUM) {
+                    // switch to another one if there is another from same queue
+                }
+                runningFor++;
+                runningProcess.increaseRuntime();
+
+                continue;
+            }
+
+            last = highestPriorityNonEmptyQueue.stream().findFirst().get();
+            runningFor = 1;
+        }
+    }
+
+    private static boolean shouldStop(Queue<Process> processes, Map<Integer, Queue<Process>> queuesByPriority) {
+        boolean noMissingProcess = processes.isEmpty();
+        boolean allQueuesAreEmpty = queuesByPriority.entrySet().stream().allMatch(entry -> entry.getValue().isEmpty());
+
+        // TODO: validate if all bursts have been spent
+
+        return noMissingProcess && allQueuesAreEmpty;
+    }
+
+    private static void putProcessesToPriorityQueue(Queue<Process> processes, int currentTime, Map<Integer, Queue<Process>> queuesByPriority) {
+        if (processes.isEmpty() || processes.peek().time() != currentTime)
+            return;
+
+        Process process = processes.poll();
+
+        queuesByPriority.get(process.priority()).add(process);
+
+        putProcessesToPriorityQueue(processes, currentTime, queuesByPriority);
     }
 
     private static Queue<Process> parseFileContentIntoProcesses(Path path) throws IOException {
--- project/src/Process.java
+++ project/src/Process.java
@@ -1,4 +1,49 @@
-public record Process(String id, int burst, int priority, int time) {
+public class Process {
+
+    private final String id;
+    private final int burst;
+    private final int priority;
+    private final int time;
+
+    private int runtime = 0;
+
+    public Process(String id, int burst, int priority, int time) {
+        this.id = id;
+        this.burst = burst;
+        this.priority = priority;
+        this.time = time;
+    }
+
+    public synchronized void increaseRuntime() {
+        if (runtime + 1 > burst)
+            throw new IllegalArgumentException();
+
+        this.runtime += 1;
+    }
+
+    public int runtime() {
+        return runtime;
+    }
+
+    public boolean isFinished() {
+        return burst == runtime;
+    }
+
+    public String id() {
+        return id;
+    }
+
+    public int burst() {
+        return burst;
+    }
+
+    public int priority() {
+        return priority;
+    }
+
+    public int time() {
+        return time;
+    }
 
     @Override
     public boolean equals(Object obj) {
